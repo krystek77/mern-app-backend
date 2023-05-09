b5cfbe893e0678701942404416feba6c3cf87179
krystek77 <krystian.wrona77@gmail.com>
2023-05-09 07:53:17 +0200

    Add endpoints to increase the amount of the category page views and get 4 most popular

--- controllers/mostPopular.js
+++ controllers/mostPopular.js
@@ -1,10 +1,25 @@
-export const increase = async(req,res) => {
-    console.log('SERVER INCREASE',req.ip);
-    console.log('',req.body)
-    const {categoryName,isCoin} = req.body;
-    const ip = req.ip;
-    res.status(200).json({message:'SERVER INCREASE'})
-}
-export const getMost = async(req,res) => {
-    console.log("SERVER GET MOST")
-}
+import Category from '../models/category.js';
+export const increase = async (req, res) => {
+  const { categoryName, isCoin } = req.body;
+  const ip = req.ip;
+  try {
+    const increase = await Category.findOneAndUpdate(
+      { slug: categoryName, coin: isCoin, ips: { $ne: ip } },
+      { $push: { ips: ip }, $inc: { pageView: 1 } },
+      { new: true }
+    );
+    if (increase) return res .status(201) .json({ message: `Increase page views for \/${categoryName}\/ and coin: \/${isCoin}\/`, });
+    return res .status(404) .json({ message: `You have already been on page: \/${categoryName}\/ and coin: \/${isCoin}\/`, });
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+export const getMost = async (req, res) => {
+  try {
+    const result = await Category.find({},{image:1,title:1,pageView:1,slug:1},{sort:{pageView:-1},limit:4})
+    res.status(200).json(result)
+} catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+  
+};
